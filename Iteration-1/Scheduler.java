7195c9f0831881d20199ecda5ac72d04148ae7e4
Jadyin <105943291+Jadyin@users.noreply.github.com>
2023-01-30 17:22:46 -0500

    Add files via upload

---
+++ Iteration-1/Scheduler.java
@@ -0,0 +1,29 @@
+
+public class Scheduler {
+	
+	
+	private boolean synchronize;
+	public boolean directionUp;
+	public int currentElevatorFloor;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public synchronized void put() {
+		
+	}
+	
+	public void tellElevator() {
+		
+	}
+	
+	public synchronized void get() {
+		
+	}
+	
+	public void createFloor() {
+			}
+
+}
