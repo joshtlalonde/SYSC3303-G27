75a84345934f6f6cc3cd4c34f0a323f2f5fd326c
Jadyin <105943291+Jadyin@users.noreply.github.com>
2023-02-01 14:35:48 -0500

    Update Scheduler.java

--- Iteration-1/Scheduler.java
+++ Iteration-1/Scheduler.java
@@ -1,3 +1,5 @@
+import java.time.*;
+
 
 public class Scheduler {
 	
@@ -5,17 +7,47 @@ public class Scheduler {
 	private boolean synchronize;
 	public boolean directionUp;
 	public int currentElevatorFloor;
+	public LocalTime time;
+	public int headingFloor;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 	
-	public synchronized void put() {
+	
+	//Do we remove synchroization
+	public synchronized void put(User_input user_input) {
+		// Prolly Start Synchronizing
+		currentElevatorFloor = user_input.getFloor();
+		headingFloor = user_input.getCar_button();
+		if(headingFloor > currentElevatorFloor) {
+			directionUp = true;
+		}
+		else if(headingFloor < currentElevatorFloor) {
+			directionUp = false;
+		}
+		else if(headingFloor == currentElevatorFloor) {
+			//At the same floor
+			//Do something
+		}
+		else {
+			System.err.println("Something with floors failed");
+			//Something went wrong 
+		}
+		
+		
+		
+		
+		
+		
+		//If statements should be here to test to see if going up or down
+		
 		
 	}
 	
 	public void tellElevator() {
+		//Pass currentFloor to Elevator
 		
 	}
 	
