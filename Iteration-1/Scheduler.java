3f368e032eecd4ad36e6a385e1eeba1efd99832e
joshtlalonde <122946454+joshtlalonde@users.noreply.github.com>
2023-02-01 22:05:40 -0500

    Updated the get and put methods
    Put method just updates the user_input variable when it's available
    Get method returns the user_input to the elevator to tell it to move to the specified floors. Then sets the user_input to null.
    Main creates and starts the threads

--- Iteration-1/Scheduler.java
+++ Iteration-1/Scheduler.java
@@ -1,61 +1,56 @@
-import java.time.*;
-
-
 public class Scheduler {
 	
+    private User_input user_input;
 	
-	private boolean synchronize;
-	public boolean directionUp;
-	public int currentElevatorFloor;
-	public LocalTime time;
-	public int headingFloor;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	
-	//Do we remove synchroization
-	public synchronized void put(User_input user_input) {
-		// Prolly Start Synchronizing
-		currentElevatorFloor = user_input.getFloor();
-		headingFloor = user_input.getCar_button();
-		if(headingFloor > currentElevatorFloor) {
-			directionUp = true;
-		}
-		else if(headingFloor < currentElevatorFloor) {
-			directionUp = false;
-		}
-		else if(headingFloor == currentElevatorFloor) {
-			//At the same floor
-			//Do something
-		}
-		else {
-			System.err.println("Something with floors failed");
-			//Something went wrong 
-		}
-		
-		
-		
-		
-		
-		
-		//If statements should be here to test to see if going up or down
-		
-		
-	}
-	
-	public void tellElevator() {
-		//Pass currentFloor to Elevator
-		
-	}
-	
-	public synchronized void get() {
-		
-	}
-	
-	public void createFloor() {
+	public synchronized void put(User_input input) {
+		while (user_input != null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println("Error waiting: " + e);
+				return;
 			}
+		}
+		
+		// Set the user_input to the one that was read by the floor
+        	user_input = input;
+		// Notify Chefs that there are ingredients on the plate
+		notifyAll();
+	}
+	
+	public synchronized User_input get() {
+        while (user_input == null) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            	System.out.println("Error waiting: " + e);
+                return null;
+            }
+		}
+		
+		// Notify Agent that the plate is empty
+		notifyAll();
+		// Copy user_input values
+		User_input input = user_input;
+		// Reset the user_input
+		user_input = null;
+		
+		// Return 
+		return input;
+	}
 
+    public static void main(String[] args) {
+        Thread floor, elevator;
+        
+        // Create table that all threads will access
+        Scheduler scheduler = new Scheduler();
+        
+        // Create Agent and Chef threads
+        floor = new Thread(new Floor(scheduler), "Floor");
+        elevator = new Thread(new Elevator(scheduler), "Elevator");
+        
+        // Start Threads
+        floor.start();
+        elevator.start();
+    }
 }
