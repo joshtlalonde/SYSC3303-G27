bd8b55aefa4b0348597cbee1134abd8b956ae074
joshtlalonde <122946454+joshtlalonde@users.noreply.github.com>
2023-02-01 22:02:37 -0500

    Changed the reading of the csv file
    There is some gross things you need to do when reading a file in java.
    Added print statements to state what data is grabbed from the csv file. Afterwards it prints what data was put into the scheduler (should be the same)
    
    This continues until the csv file is completed

--- Iteration-1/Floor.java
+++ Iteration-1/Floor.java
@@ -1,63 +1,88 @@
-package iteration1;
-import java.time.*;
 import java.io.*;
-import java.util.Scanner;
-//import Scheduler;
 
 public class Floor implements Runnable {
-	private User_input user_input;
-	private User_input[] user_input_list;
-	//private Scheduler schedule;
+	private Scheduler scheduler;
 	
-	public Floor(User_input user_input) {
-		this.user_input = user_input;
+	/** Constructor for Floor */
+	public Floor(Scheduler scheduler) {
+		this.scheduler = scheduler;	
+	}
+	
+	public User_input csv_to_user(String line) {
+		String[] words = line.split(",");
 		
+		// Convert strings to their appropriate types
+		//LocalTime t = LocalTime.parse(words[0], DateTimeFormatter.ISO_LOCAL_TIME);
+		String t = words[0];
+		int f = Integer.parseInt(words[1]);
+		Boolean fb = Boolean.parseBoolean(words[2]);
+		int cb = Integer.parseInt(words[3]);
+
+		// Create new user
+		return new User_input(t, f, fb, cb);
 	}
-	public User_input[] getUser_input() {
-		return user_input_list;
-	}
-	Scanner scan = new Scanner(new File("D:\\Books\\Third Year\\Winter\\Sysc 3303 B\\Project IT1\\iteration1\\src\\iteration1\floor_input.csv"));
-	scan.useDelimiter(",");
-	for(int i = 0; i<4;i++) {
-		while(scan.hasNext()) { 
-			LocalTime t = scan.next();
-			int f = scan.next();
-			int fb = scan.next();
-			int cb = scan.next();
-			user_input(t,f,fb,cb);
-			
-		
-	}
-	}
+
+	/** Function to be run on Thread.start() */
 	public void run() {
-		//schedule.put(user_input);
-		try {} catch (InterruptedException e) {}
-	}
-	private void user_input(LocalTime t, int f, int fb, int cb) {
-		// TODO Auto-generated method stub
+		// Open and read file line-by-line
+		BufferedReader reader;
+		try {
+			String line;
+			reader = new BufferedReader(new FileReader("../floor_input.txt"));
+			
+			while((line = reader.readLine()) != null) {
+				User_input user_input = csv_to_user(line);
+				System.out.println(Thread.currentThread().getName() + " got " + user_input + " from the csv file");
+
+				// Puts the user_input into the scheduler
+				scheduler.put(user_input);
+				System.out.println(Thread.currentThread().getName() + " put " + user_input + " into the scheduler");
+
+				// Sleep for 1 second
+				try {
+					Thread.sleep(1000); 
+				} catch (InterruptedException e) {}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Failed to open File: " + e);
+			return;
+		} catch (IOException e) {
+			System.out.println("Failed to read File: " + e);
+			return;
+		}
 		
+		// Close reader
+		try {
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("Failed to close File: " + e);
+		}
 	}
-	
-	
 }
+
 class User_input{
-	private LocalTime time;
+	//private LocalTime time;
+	private String time;
 	private int floor;
 	private boolean floor_button;
 	private int car_button;
 	
 	
 	//Making the constructor for the user input class
-	public User_input(LocalTime time, int floor, boolean floor_button, int car_button) {
+	public User_input(String time, int floor, boolean floor_button, int car_button) {
 		this.time = time;
 		this.floor = floor;
 		this.floor_button= floor_button;
 		this.car_button = car_button;
-		
+	}
+	
+	@Override
+	public String toString() {
+		return "{time: " + time + ", floor: " + floor + ", floor_button: " + floor_button + ", car_button: " + car_button + "}";
 	}
 	
 	//Getting the data from the user input
-	public LocalTime getTime() {
+	public String getTime() {
 		return time;
 	}
 	public int getFloor() {
