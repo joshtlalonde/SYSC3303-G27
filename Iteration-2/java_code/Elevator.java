e7c4cca20d029cf0d5c423db54c9c21b54a06700
joshtlalonde <122946454+joshtlalonde@users.noreply.github.com>
2023-02-26 12:52:29 -0500

    Create Elevator.java

---
+++ Iteration-2/java_code/Elevator.java
@@ -0,0 +1,34 @@
+//Program for Elevator (consumer)
+class Elevator implements Runnable
+{
+    private int floor;
+    private boolean directionUp;
+    private Scheduler scheduler;
+    
+    public Elevator(Scheduler scheduler)
+    {
+        this.scheduler = scheduler;
+    }
+
+    public void run()
+    {
+        while(true){
+            System.out.println("Elevator: Ready on floor " + floor);
+            User_input user_input = scheduler.get();
+            
+            System.out.println("Elevator: Moved to " + user_input.getCar_button() + " floor");
+            
+            // Set the floor elevator is on
+            floor = user_input.getCar_button();
+            // Set direction elevator is moving
+            directionUp = user_input.getFloor_button();
+
+            // Sleep for 1 second
+            try {
+	            Thread.sleep(1000); 
+	        } catch (InterruptedException e) {
+			System.exit(1);
+		 }
+        }
+    }
+}
