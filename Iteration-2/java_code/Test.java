6dbb72d88e7bf8c769b341a7cb2785bcef85c9ea
joshtlalonde <122946454+joshtlalonde@users.noreply.github.com>
2023-02-27 03:01:38 -0500

    Renaming functions to match new implementation

--- Iteration-2/java_code/Test.java
+++ Iteration-2/java_code/Test.java
@@ -1,6 +1,7 @@
 public class Test extends junit.framework.TestCase{
 	private Scheduler scheduler = new Scheduler(); 
-	private Floor floor = new Floor(scheduler);
+	DirectionLamp directionLamp = new DirectionLamp();
+	private Floor floor = new Floor(scheduler, directionLamp);
 
 	public Test ()
 	{
@@ -9,11 +10,11 @@ public class Test extends junit.framework.TestCase{
 	
 	public void testReading()
 	{
-		User_input ui = new User_input("10:05:15.0",2,true,4);
-		User_input testui = floor.file_to_user("10:05:15.0,2,Up,4");
+		UserInput ui = new UserInput("10:05:15.0",2,true,4);
+		UserInput testui = floor.fileToUser("10:05:15.0,2,Up,4");
 		assertEquals(ui.getTime(), testui.getTime());
 		assertEquals(ui.getFloor(), testui.getFloor());
-		assertEquals(ui.getFloor_button(), testui.getFloor_button());
-		assertEquals(ui.getCar_button(), testui.getCar_button());
+		assertEquals(ui.getFloorButton(), testui.getFloorButton());
+		assertEquals(ui.getCarButton(), testui.getCarButton());
 	}
 }
