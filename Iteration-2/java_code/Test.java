022dee06990c3ceaf74e95e99a960b54bb181312
joshtlalonde <122946454+joshtlalonde@users.noreply.github.com>
2023-02-26 22:17:18 -0500

    Added assertEquals

--- Iteration-2/java_code/Test.java
+++ Iteration-2/java_code/Test.java
@@ -1,5 +1,6 @@
 public class Test extends junit.framework.TestCase{
-	private Floor floor;
+	private Scheduler scheduler = new Scheduler(); 
+	private Floor floor = new Floor(scheduler);
 
 	public Test ()
 	{
@@ -9,8 +10,10 @@ public class Test extends junit.framework.TestCase{
 	public void testReading()
 	{
 		User_input ui = new User_input("10:05:15.0",2,true,4);
-		User_input testui = floor.file_to_user("'10:05:15.0',2,true,4");
-		assertEquals(ui,testui);
+		User_input testui = floor.file_to_user("10:05:15.0,2,Up,4");
+		assertEquals(ui.getTime(), testui.getTime());
+		assertEquals(ui.getFloor(), testui.getFloor());
+		assertEquals(ui.getFloor_button(), testui.getFloor_button());
+		assertEquals(ui.getCar_button(), testui.getCar_button());
 	}
-
 }
