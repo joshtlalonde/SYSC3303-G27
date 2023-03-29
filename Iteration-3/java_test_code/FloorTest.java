187b1e0afde80ac8c5d8b2db0f950d4f9f262fa4
Josh <joshtlalonde@cmail.carleton.ca>
2023-03-29 16:43:27 -0400

    Created Test File for Floor

---
+++ Iteration-3/java_test_code/FloorTest.java
@@ -0,0 +1,20 @@
+import java.net.*;
+import java.text.*;
+import java.util.*;
+import org.junit.*;
+import org.junit.Assert.*;
+
+public class FloorTest extends junit.framework.TestCase {
+    
+    public UserInput testFileToUser(String line) {
+
+    }
+
+    public void testButtonPress(boolean floorButtonUp, int floor) {
+
+    }
+
+    public void testElevatorArrival(boolean elevatorDirection, int floor) {
+
+    }
+}
