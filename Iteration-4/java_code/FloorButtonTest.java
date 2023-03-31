5e99a19531a265d49046d991774d3df77faf72e4
sanyapeter <123699869+sanyapeter@users.noreply.github.com>
2023-03-31 03:23:43 -0400

    Update FloorButtonTest.java

--- Iteration-4/java_code/FloorButtonTest.java
+++ Iteration-4/java_code/FloorButtonTest.java
@@ -5,15 +5,13 @@ import org.junit.*;
 import org.junit.Assert.*;
 
 public class FloorButtonTest extends junit.framework.TestCase {
-    
-  
-    FloorLamp floorlamp = new FloorLamp();
-		FloorButton floorButton = new FloorButton(5);
-		floorButton.buttonLamp = floorlamp;
-		assertFalse(floorButton.getButtonState());
-		floorButton.pressUp();
-		assertTrue(floorButton.getButtonState());
-		assertTrue(floorButton.getButtonDirectionUp());
+	FloorLamp floorlamp = new FloorLamp();
+	FloorButton floorButton = new FloorButton(5);
+	floorButton.buttonLamp = floorlamp;
+	assertFalse(floorButton.getButtonState());
+	floorButton.pressUp();
+	assertTrue(floorButton.getButtonState());
+	assertTrue(floorButton.getButtonDirectionUp());
   
   
 }
