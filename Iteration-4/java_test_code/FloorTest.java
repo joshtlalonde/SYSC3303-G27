0d557f17d35c7cde4c868013d1a5a12bb8d37a1f
sanyapeter <123699869+sanyapeter@users.noreply.github.com>
2023-03-31 15:19:04 -0400

    Update FloorTest.java

--- Iteration-4/java_test_code/FloorTest.java
+++ Iteration-4/java_test_code/FloorTest.java
@@ -5,16 +5,34 @@ import org.junit.*;
 import org.junit.Assert.*;
 
 public class FloorTest extends junit.framework.TestCase {
+    public void testConstructor() {
+    
+        Floor floor = new Floor();
+    
+        //checking floor button initialisation
+        assertEquals(Floor.NUMBER_OF_FLOORS, floor.floorButton.size());
+        //checking floor requests initialisation
+	    assertEquals(Floor.NUMBER_OF_FLOORS, floor.floorRequests.size());
     
     public UserInput testFileToUser(String line) {
 
     }
 
     public void testButtonPress(boolean floorButtonUp, int floor) {
+        Floor floor = new Floor();
+        floor.buttonPress(true, 10);//pressed up button
+		assertTrue(floor.floorButton.get(4).getUpButton());
+        floor.buttonPress(false, 15);//pressed down button
+        assertTrue(floor.floorButton.get(4).getDownButton());
 
     }
 
     public void testElevatorArrival(boolean elevatorDirection, int floor) {
+        
+        // verifying if the button was reset
+        assertEquals(false, floor.floorButton.get(5).getUpButton());
+        // verify if the floor requests were removed from array
+        assertEquals(true, floor.floorRequests.isEmpty());
 
     }
 }
