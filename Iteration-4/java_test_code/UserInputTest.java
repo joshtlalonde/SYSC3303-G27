1977d6eef987617cac9656bda2fab5e9180be08f
sanyapeter <123699869+sanyapeter@users.noreply.github.com>
2023-04-01 17:01:23 -0400

    Update UserInputTest.java

--- Iteration-4/java_test_code/UserInputTest.java
+++ Iteration-4/java_test_code/UserInputTest.java
@@ -1,3 +1,3 @@
 public class UserInputTest extends junit.framework.TestCase{
   public void testConstructor() {
-    
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss.SSS", Locale.ENGLISH);
