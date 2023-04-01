2fdb48376dcaa17850d606d566e8b9f29de934cc
sanyapeter <123699869+sanyapeter@users.noreply.github.com>
2023-04-01 18:00:08 -0400

    Update UserInputTest.java

--- Iteration-4/java_test_code/UserInputTest.java
+++ Iteration-4/java_test_code/UserInputTest.java
@@ -1,9 +1,9 @@
 public class UserInputTest extends junit.framework.TestCase{
   public void testConstructor() {
-    SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss.SSS", Locale.ENGLISH);
+	  SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss.SSS", Locale.ENGLISH);
     
-        }
-    UserInput userinput = new UserInput(12:10:09.5, 4, true, 8, false, true);
+        
+    	  UserInput userinput = new UserInput(12:10:09.5, 4, true, 8, false, true);
     try {
             assertEquals(userinput.getTime(), dateFormatter.parse("12:10:09.5"));
         } catch (ParseException e) {
@@ -29,4 +29,13 @@ public void testConvertPacket() {
         assertFalse(userInput.getDoorFault());
         assertTrue(userInput.getHardFault());
 }
-        
+public void testconvertToBytes() throws ParseException {
+	SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss.SSS", Locale.ENGLISH);
+	UserInput userinput = new UserInput(12:10:09.5, 4, true, 8, false, true);
+	byte[] expectedBytes = new byte[] {0x0C, 0x22, (byte) 0xB8, (byte) 0xD9, 0x00, 0x03, 0x01, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x01
+		};
+		
+	assertArrayEquals(expectedBytes, userinput.convertToBytes());
+	
+	)
+	
