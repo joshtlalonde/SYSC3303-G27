4492283ed5414fc2f67a25de448ac91c4d5d62d0
sanyapeter <123699869+sanyapeter@users.noreply.github.com>
2023-04-01 19:29:24 -0400

    Update FloorLampTest.java

--- Iteration-4/java_test_code/FloorLampTest.java
+++ Iteration-4/java_test_code/FloorLampTest.java
@@ -1,18 +1,29 @@
 public class FloorLampTest extends junit.framework.TestCase{
-  //private boolean lampState = false;
-  public LampTest()
+	//private boolean lampState = false;
+  	public LampTest()
 	{
 		
 		 
 	}
 	
-	public void testFloorLamp()
-	{
+	public void testTurnOn() {
 		FloorLamp floorlamp = new FloorLamp();
+		floorlamp.turnOn();
+		assertTrue(floorlamp.getLampState());
+		}
+		
+	public void testTurnOff() {
+		FloorLamp floorlamp = new FloorLamp();
+		floorlamp.turnOff();
 		assertFalse(floorlamp.getLampState());
+	}
+	public void testGetLampState() {
+		FloorLamp floorlamp = new FloorLamp();
 		floorlamp.turnOn();
 		assertTrue(floorlamp.getLampState());
 		floorlamp.turnOff();
-		assertFale(floorlamp.getLampState());
+		assertFalse(floorlamp.getLampState());
+	}
+
     
   }
