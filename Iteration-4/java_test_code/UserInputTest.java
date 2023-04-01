f08035a3b2e179c302b16dc39e9c9cac7d397288
sanyapeter <123699869+sanyapeter@users.noreply.github.com>
2023-04-01 17:09:28 -0400

    Update UserInputTest.java

--- Iteration-4/java_test_code/UserInputTest.java
+++ Iteration-4/java_test_code/UserInputTest.java
@@ -2,14 +2,7 @@ public class UserInputTest extends junit.framework.TestCase{
   public void testConstructor() {
     SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss.SSS", Locale.ENGLISH);
     UserInput userinput = new UserInput(time, 4, true, 8, false, true);
-     Date date;
-        try {
-            date = dateFormatter.parse("10:10:10.5");
-        } catch (ParseException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-            return;
-        }
+    
     try {
             assertEquals(userinput.getTime(), dateFormatter.parse("10:10:10.5"));
         } catch (ParseException e) {
