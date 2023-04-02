76fa97fdf1e413fcefb59c3ea0907423f24469d7
sanyapeter <123699869+sanyapeter@users.noreply.github.com>
2023-04-02 19:41:48 -0400

    Update FloorTest.java

--- Iteration-4/java_test_code/FloorTest.java
+++ Iteration-4/java_test_code/FloorTest.java
@@ -5,60 +5,65 @@ import org.junit.*;
 import org.junit.Assert.*;
 
 public class FloorTest extends junit.framework.TestCase {
-    public void testConstructor() {
+	public void testConstructor() {
     
         Floor floor = new Floor();
     
         //checking floor button initialisation
         assertEquals(Floor.NUMBER_OF_FLOORS, floor.floorButton.size());
         //checking floor requests initialisation
-	    assertEquals(Floor.NUMBER_OF_FLOORS, floor.floorRequests.size());
+	assertEquals(Floor.NUMBER_OF_FLOORS, floor.floorRequests.size());
+	}
     
-    public UserInput testFileToUser() {
-	Floor floor = new Floor();
-	UserInput userinput = floor.fileToUser("04:10:30.200,5,Up,16,true,false");
-    	assertEquals(userinput.getCurrentFloor(),5);
-    	assertTrue(userinput.getFloorButtonUp());
-    	assertEquals(userinputt.getDestinationFloor(),16);
-    	assertTrue(userinput.getDoorFault());
-    	assertFalse(userinput.getHardFault());
-}
+ 	public UserInput testFileToUser() {
+		Floor floor = new Floor();
+		UserInput userinput = floor.fileToUser("04:10:30.200,5,Up,16,true,false");
+    		assertEquals(userinput.getCurrentFloor(),5);
+    		assertTrue(userinput.getFloorButtonUp());
+    		assertEquals(userinputt.getDestinationFloor(),16);
+    		assertTrue(userinput.getDoorFault());
+    		assertFalse(userinput.getHardFault());
+		}
 	    
 
-    }
-
-    public void testButtonPress() {
-        Floor floor = new Floor();
-        floor.buttonPress(true, 10);//pressed up button
-	assertTrue(floor.floorButton.get(4).getUpButton());
-        floor.buttonPress(false, 15);//pressed down button
-        assertTrue(floor.floorButton.get(4).getDownButton());
-
-    }
-
-    public void testElevatorArrival() throws Exception {
-	Floor floor = new Floor();
-	UserInput userinput = floor.fileToUser("04:10:30.200,5,Up,16,true,false");
-	floor.floorRequests.add(userInput);
-        floor.elevatorArrival(userInput);
-        
-        // verifying if the button was reset
-        assertEquals(false, floor.floorButton.get(5).getUpButton());
-        // verify if the floor requests were removed from array
-        assertEquals(true, floor.floorRequests.isEmpty());
-
-    }
     
-    public void testSendFloorRequest() throws Exception{
-    	Floor floor = new Floor();
-	InetAddress localAddr;
-		try {
-			localAddr = InetAddress.getLocalHost();
-		} catch (UnknownHostException e) {
+
+    	public void testButtonPress() {
+        	Floor floor = new Floor();
+        	floor.buttonPress(true, 10);//pressed up button
+		assertTrue(floor.floorButton.get(4).getUpButton());
+        	floor.buttonPress(false, 15);//pressed down button
+        	assertTrue(floor.floorButton.get(4).getDownButton());
+
+    }
+	
+	public void testSendFloorRequest() throws Exception{
+    		Floor floor = new Floor();
+		InetAddress localAddr;
+			try {
+				localAddr = InetAddress.getLocalHost();
+			} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
+				e.printStackTrace();
+				return;
 		}
     }
-    
+    	public void testReceiveFloorResponse() throws Exception{
+    	
 }
+
+
+	public void testElevatorArrival() throws Exception {
+		Floor floor = new Floor();
+		UserInput userinput = floor.fileToUser("04:10:30.200,5,Up,16,true,false");
+		floor.floorRequests.add(userinput);
+        	floor.elevatorArrival(userinput);
+        
+        	// verifying if the button was reset
+        	assertEquals(false, floor.floorButton.get(5).getUpButton());
+        	// verify if the floor requests were removed from array
+        	assertEquals(true, floor.floorRequests.isEmpty());
+
+    }
+    
+    
