d17ea6e0c46dfcb17879fa497367d4ae0653cc90
Jadyin <105943291+Jadyin@users.noreply.github.com>
2023-04-02 15:11:38 -0400

    Update ElevatorGUI.java

--- Iteration-4/java_code/ElevatorGUI.java
+++ Iteration-4/java_code/ElevatorGUI.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.GridBagConstraints;
@@ -14,16 +15,34 @@ public class ElevatorGUI extends JFrame{
 	JLabel[] elevatorNumbers;
 	JLabel[] elevatorStates;
 	JLabel[] numRequests;
+	Elevator_GUI_Image[] ELE;
+	JPanel[] Backgrounds;
+	
+	private int RECT_X;
+	private int RECT_Y;
+	private int RECT_HEIGHT;
+	private int RECT_WIDTH;
 	
 	
 	// create the frame and initialize the GUI
 	public ElevatorGUI(int numElevators, int numFloors) {
 		
+		//Backgrounds[0] = new JPanel();
 		//Initilization
+		/* setRectangle();
+		ELE = new Elevator_GUI_Image[numElevators];
+		Backgrounds = new JPanel[numElevators+2];
+		Backgrounds[0] = new JPanel();
+		for(int i = 0; i < numElevators; i++) {
+			ELE[i] = new Elevator_GUI_Image(Color.gray, i);
+			Backgrounds[i+1] = new JPanel();
+			Backgrounds[i+1].add(ELE[i]);
+		}
+		Backgrounds[1 + numElevators] = new JPanel();
 		JFrame frame = new JFrame();
 		GridBagLayout GBLayout = new GridBagLayout();
 		GridBagConstraints GBConstraints = new GridBagConstraints();
-		JPanel Background = new JPanel();
+		
 		
 		//Frame setup
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,66 +50,79 @@ public class ElevatorGUI extends JFrame{
 		
 		
 		//Background Setup
-		Background.setForeground(new Color(193, 193, 255));
-		Background.setBackground(new Color(193, 193, 255));
-		Background.setBorder(new EmptyBorder(5,5,5,5));
-		Background.setLayout(GBLayout); 
+		Backgrounds[0].setForeground(new Color(193, 193, 255));
+		Backgrounds[0].setBackground(new Color(193, 193, 255));
+		Backgrounds[0].setBorder(new EmptyBorder(5,5,5,5));
+		Backgrounds[0].setLayout(GBLayout);
 		
-		frame.setContentPane(Background); 
+		frame.setContentPane(Backgrounds[0]); 
 		
-		
-		sliders = new JSlider[numElevators];
 		elevatorNumbers = new JLabel[numElevators];
 		elevatorStates = new JLabel[numElevators];
 		numRequests = new JLabel[numElevators];
-		
-		for(int i = 0; i < numElevators; i++) {
-			sliders[i] = new JSlider();
-			sliders[i].setValue(1); 
-			sliders[i].setOrientation(SwingConstants.VERTICAL);
-			sliders[i].setMinimum(1); 
-			sliders[i].setMaximum(numFloors); 
-			sliders[i].setMinorTickSpacing(0);
-			sliders[i].setMajorTickSpacing(1); 
-			sliders[i].setPaintTicks(false); 
-			sliders[i].setPaintLabels(true);
-			GBConstraints.fill = GridBagConstraints.HORIZONTAL;
-			GBConstraints.gridx = i;
-	        GBConstraints.gridy = 0;
-	        Background.add(sliders[i], GBConstraints); // add the slider to the content pane
-	        
-	       
-	        //create a lable under the slider which say the elevators name
-	        elevatorNumbers[i] = new JLabel("Elevator " + Integer.toString(1+i));
-			// format where the label will go (next section in horizontal row 1)
-			GBConstraints.fill = GridBagConstraints.HORIZONTAL;
-			GBConstraints.gridx = i;
-	        GBConstraints.gridy = 1;
-	        Background.add(elevatorNumbers[i], GBConstraints);
-			
-			//create a lable under the slider which say the elevators current state
-			elevatorStates[i] = new JLabel("State: IDLE         ");  
-			// format where the label will go (next section in horizontal row 2)
-			GBConstraints.fill = GridBagConstraints.HORIZONTAL;
-			GBConstraints.gridx = i;
-	        GBConstraints.gridy = 2;
-	        Background.add(elevatorStates[i], GBConstraints);
-			
-			//create a lable under the slider which say the elevators current # of requests
-			numRequests[i] = new JLabel("# Requests: 0       ");
-			// format where the label will go (next section in horizontal row 3)
-			GBConstraints.fill = GridBagConstraints.HORIZONTAL;
-			GBConstraints.gridx = i;
-	        GBConstraints.gridy = 3;
-	        Background.add(numRequests[i], GBConstraints);
-			
-		}
 		   
+		frame.setVisible(true);
+		*/
+		//Backgrounds[0] = new JPanel();
+		//Initilization
+		setRectangle();
+		ELE = new Elevator_GUI_Image[numElevators];
+		Backgrounds = new JPanel[numElevators+2];
+		Backgrounds[0] = new JPanel();
+		for(int i = 0; i < numElevators; i++) {
+			ELE[i] = new Elevator_GUI_Image(Color.gray, i);
+			Backgrounds[i+1] = new JPanel();
+			Backgrounds[i+1].add(ELE[i]);
+		}
+		Backgrounds[1 + numElevators] = new JPanel();
+		JFrame frame = new JFrame();
+		GridBagLayout GBLayout = new GridBagLayout();
+		GridBagConstraints GBConstraints = new GridBagConstraints();
+		JPanel Background = new JPanel();
+		
+			
+		//Frame setup
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 450, 300);
+			
+			
+		//Background Setup
+		Background.setForeground(new Color(193, 193, 255));
+		Background.setBackground(new Color(193, 193, 255));
+		Background.setBorder(new EmptyBorder(5,5,5,5));
+		Background.setLayout(GBLayout);
+		
+		
+		for(int i = 0; i< (numElevators + 2); i++) {
+			Background.add(Backgrounds[i]);
+		}
+		
+		
+	
+		//Add the left and right side panels
+		//JPanel leftPanel = new JPanel();
+		//JPanel rightPanel = new JPanel();
+		//leftPanel.setBackground(Color.white);
+		//rightPanel.setBackground(Color.white);
+			
+		//Add the elevator panels in between the left and right side panels		
+		frame.setContentPane(Background);  
 		frame.setVisible(true);
 	}
 	
+	
+	
+	
 	public void setSlider(int elevatorNum, int floorNum) {
 		sliders[elevatorNum-1].setValue(floorNum);
+	}
+	
+	public void setRectangle() {
+		//Can make changes to Elevator Rectangles in here if need to
+		RECT_X = 20; 
+		RECT_Y = RECT_X; 
+		RECT_WIDTH = 50; 
+		RECT_HEIGHT = 100;
 	}
 	
 	public void setState(int elevatorNum , Elevator_State currentState) {		
@@ -123,6 +155,10 @@ public class ElevatorGUI extends JFrame{
 		// update the elevator's state label
 		numRequests[elevatorNum-1].setText("# Requests: " + newNumberRequests + "       ");
 	}
+	
+	public static void main(String[] args) {
+		ElevatorGUI GUI = new ElevatorGUI(4,20);
+		
+	}
+	
 }
-
-
