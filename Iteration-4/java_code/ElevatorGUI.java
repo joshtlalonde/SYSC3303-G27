c120a288b23967e16520d0c7c08f5765e05aa4de
Sanskar <44382328+Sigma-343@users.noreply.github.com>
2023-04-03 00:04:42 -0400

    Update ElevatorGUI.java
    Re-hauled GUI. Current issue: "Floor labels do not show up". Functionality still needs to be added.

--- Iteration-4/java_code/ElevatorGUI.java
+++ Iteration-4/java_code/ElevatorGUI.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -68,34 +73,73 @@ public class ElevatorGUI extends JFrame{
 		//Initilization
 		setRectangle();
 		
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setSize(1200, 1200);
+		frame.getContentPane().setBackground(Color.cyan); 
+		//frame.setLayout(new GridLayout(21, 6));
+		//frame.setBounds(500, 500, 550, 500);
+		
+		
 		ELE = new Elevator_GUI_Image[numElevators];
 		elevatorBackground = new JPanel[numElevators];
+		int spacingWidth = 200;
 
 		for(int i = 0; i < numElevators; i++) {
 			ELE[i] = new Elevator_GUI_Image(Color.gray, i);
-			elevatorBackground[i] = new JPanel(new GridLayout(21,1));
+			elevatorBackground[i] = new JPanel(new GridLayout(21,1, 100, 0));
 			elevatorBackground[i].add(ELE[i], new GridBagConstraints());
+			elevatorBackground[i].setBounds(spacingWidth, 0, 180, 1200);
+			spacingWidth+=200;
+			frame.add(elevatorBackground[i]);
 		}
-		
-		LeftPanel = new JPanel(new GridLayout(21, 1));
-		RightPanel = new JPanel(new GridLayout(21,1));
+	
+		LeftPanel = new JPanel(new BorderLayout());
+		RightPanel = new JPanel();
 		LeftPanel.setBackground(Color.black);
+		//LeftPanel.setPreferredSize(new Dimension(200, 1200));
 		
 		//LeftPanel.setSize(getPreferredSize());
 		RightPanel.setBackground(Color.black);
+		LeftPanel.setBounds(0, 0, 200, 1200);
+		RightPanel.setBounds(1000, 0, 200, 1200);
+		frame.add(LeftPanel);
+		frame.add(RightPanel);
+		
+		int hincrement = 90;
+		int hposition = 200;
+		int vposition = 57;
+		for(int x = 0; x < 21; x++) {
+			JLabel label = new JLabel();
+			label.setText(String.valueOf(x));
+			label.setFont(new Font("MV Boli", Font.PLAIN, 40));
+			label.setBackground(Color.red);
+			for(int y = 1; y < 5; y++) {
+				hposition += (y * hincrement);
+				label.setLocation(hposition, vposition);
+				LeftPanel.add(label);
+			}
+			vposition+= 57;
+		}
+		
+		frame.setVisible(true);
+		
 		//RightPanel.setSize(getPreferredSize());
 		
-		JFrame frame = new JFrame();
+		/*
 		JPanel Background = new JPanel();
 		
 			
 		//Frame setup
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(500, 500, 550, 500);
+
 			
 		//Background Setup
 		Background.setBackground(Color.cyan);
-		Background.setBorder(new EmptyBorder(5,5,5,5));
+		Background.setBorder(new EmptyBorder(0,0,1200,1200));
+		
+		
 		Background.add(LeftPanel);
 		
 		
@@ -103,7 +147,7 @@ public class ElevatorGUI extends JFrame{
 		for(int i = 0; i< numElevators; i++) {
 			Background.add(elevatorBackground[i]);
 		}
-		Background.add(RightPanel);
+		Background.add(RightPanel);*/
 		
 		
 	
@@ -114,9 +158,7 @@ public class ElevatorGUI extends JFrame{
 		//rightPanel.setBackground(Color.white);
 			
 		//Add the elevator panels in between the left and right side panels	
-		frame.setContentPane(Background);  
-		frame.pack();
-		frame.setVisible(true);
+
 	}
 	
 	
