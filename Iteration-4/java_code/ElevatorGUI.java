41be2087e55f82c50d0889f9f185adacda8c1508
joshbob999 <joshbob38@gmail.com>
2023-04-09 20:01:35 -0400

    Adding in number of passengers

--- Iteration-4/java_code/ElevatorGUI.java
+++ Iteration-4/java_code/ElevatorGUI.java
@@ -6,6 +6,7 @@ public class ElevatorGUI extends JFrame {
 
     private static JLabel[] elevatorStatusLabel;
     private static JLabel[] elevatorCurrentStatus;
+    private static JLabel[] elevatorPassengers;
     private static ElevatorPanel[] elevatorPanels;
 
     public ElevatorGUI() {
@@ -18,14 +19,17 @@ public class ElevatorGUI extends JFrame {
         // Create the elevator status panels
         elevatorStatusLabel = new JLabel[4];
         elevatorCurrentStatus = new JLabel[4];
+        elevatorPassengers = new JLabel[4];
         JPanel[] elevatorStatusPanels = new JPanel[4];
         for (int i = 0; i < 4; i++) {
             elevatorStatusLabel[i] = new JLabel("Elevator " + (i + 1) + " is on floor 0");
-            elevatorCurrentStatus[i] = new JLabel("Moving up");
+            elevatorCurrentStatus[i] = new JLabel("Idle");
+            elevatorPassengers[i] = new JLabel("0");
             elevatorStatusPanels[i] = new JPanel();
-            elevatorStatusPanels[i].setLayout(new GridLayout(2, 1));
+            elevatorStatusPanels[i].setLayout(new GridLayout(3, 1));
             elevatorStatusPanels[i].add(elevatorStatusLabel[i]);
             elevatorStatusPanels[i].add(elevatorCurrentStatus[i]);
+            elevatorStatusPanels[i].add(elevatorPassengers[i]);
             add(elevatorStatusPanels[i]);
         }
 
@@ -40,15 +44,17 @@ public class ElevatorGUI extends JFrame {
         setVisible(true);
     }
 
-    public void updateStatus(int elevatorId, int currentFloor, String state) {
+    public void updateStatus(int elevatorId, int currentFloor, String state, int numPassengers) {
         elevatorStatusLabel[elevatorId].setText("Elevator " + (elevatorId + 1) + " is on floor " + currentFloor);
-        elevatorCurrentStatus[elevatorId].setText(state);
+        elevatorCurrentStatus[elevatorId].setText("Elevator " + (elevatorId + 1)  + " is in state " + state);
+        elevatorPassengers[elevatorId].setText("Elevator " + (elevatorId + 1)  + " has " + numPassengers + " passengers");
         elevatorPanels[elevatorId].setCurrentFloor(currentFloor);
         elevatorPanels[elevatorId].repaint();
     }
 
     public static void main(String[] args) {
         ElevatorGUI guiTestEle = new ElevatorGUI();
+        int passengers = 0;
         // Call the updateStatus method periodically to update the GUI based on the current state of the elevators
         while (true) {
             // Get the current state of each elevator
@@ -71,13 +77,14 @@ public class ElevatorGUI extends JFrame {
                 }
                 else {
                 	setDestinationFloor(i,(int)(Math.random() * 10));
+                    passengers = (int)(Math.random() * 10);
                 	isMovingUp = false;
                 	
                 }
                 
                String state = "idle";
         
-               guiTestEle.updateStatus(i, currentFloor, state);
+               guiTestEle.updateStatus(i, currentFloor, state, passengers);
             }
 
             // Wait for a short time before updating again
@@ -105,13 +112,14 @@ public class ElevatorGUI extends JFrame {
     }
     
     private static void setDestinationFloor(int elevatorId, int destinationFloor) {
-    		elevatorPanels[elevatorId].destinationFloor = destinationFloor;
+    	elevatorPanels[elevatorId].destinationFloor = destinationFloor;
     }
 
     private class ElevatorPanel extends JPanel {
         private int currentFloor = 0;
         private boolean isMovingUp = false;
         private int destinationFloor = 5;
+        private int numPassengers = 0;
         
 
         public void setCurrentFloor(int currentFloor) {
